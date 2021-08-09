3ee8ed1efba86c4e8decc61604a961647638e97d
mazurkate8 <mazurkate8@gmail.com>
2021-08-09 12:17:42 +0300

    Сделала задачу 3

--- src/main/java/com/home/work/task/three/TaskThree.java
+++ src/main/java/com/home/work/task/three/TaskThree.java
@@ -1,4 +1,35 @@
 package com.home.work.task.three;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TaskThree {
+    public static String sortArray(String[] arr){
+        List<String> list =  Arrays.asList(arr);
+
+        String tmp =  list.stream()
+                .map(n->n.replace(",,", ","))
+                .collect(Collectors.toSet())
+                .stream()
+                .sorted()
+                .collect(Collectors.joining(","))
+                .replaceAll(" ", "");
+
+        String[] tmpArr = tmp.split(",");
+
+        return Arrays.stream(tmpArr)
+                .map(Integer::valueOf)
+                .collect(Collectors
+                .toList())
+                .stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .stream().map(String::valueOf)
+                .collect(Collectors.joining(", "));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortArray(new String[]{"1, 200, 0", "400, 5", "7, 6669"}));
+    }
 }
